a4f7cdef3dc3cb89ae16458bd14200126873e099
Weyaxi <81961593+Weyaxi@users.noreply.github.com>
2021-05-05 23:04:53 +0300

    Add files via upload

---
+++ takvim.py
@@ -0,0 +1,13 @@
+import calendar
+
+print("------------------")
+yıl = int(input("Yıl giriniz: "))
+ay = int(input("Ay Giriniz: "))
+print("------------------")
+
+print("\n")
+
+print("-------------------------- \n")
+print(calendar.month(yıl, ay))
+
+print("--------------------------")
