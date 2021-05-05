e599a57b7de36737081a5df4d74ecdaa2bb0f1ec
Weyaxi <81961593+Weyaxi@users.noreply.github.com>
2021-05-05 23:19:24 +0300

    Update takvim.py

--- takvim.py
+++ takvim.py
@@ -1,13 +1,30 @@
 import calendar
+import sys
 
 print("------------------")
-yıl = int(input("Yıl giriniz: "))
-ay = int(input("Ay Giriniz: "))
-print("------------------")
 
-print("\n")
+try:
+    yıl = int(input("Yıl giriniz: "))
+except:
+    print("Bir hata ile karşılaşıldı. Lütfen yıl değerini, geçerli bir değer olarak giriniz.")    
+    sys.exit()
+try:
+    ay = int(input("Ay Giriniz: "))
+except:
+    print("Bir hata ile karşılaşıldı. Lütfen ay değerini, geçerli bir değer olarak giriniz.")   
+    sys.exit()     
+    
+print("------------------ \n")
+
 
 print("-------------------------- \n")
-print(calendar.month(yıl, ay))
+
+try:
+    print(calendar.month(yıl, ay))
+except:
+    print("Bir hata ile karşılaşıldı. Lütfen söz konusu yıl ve ay değerlerini, geçerli bir değer olarak girdiğinizden emin olunuz.")   
+    print("--------------------------")
+    sys.exit()         
+
 
 print("--------------------------")
